62e4115709f0987efe6f4b1022ec1247665c9e63
amantusher6-byte <amantusher6@gmail.com>
2026-08-16 04:19:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2098,6 +2098,31 @@ function clearAllTransactions() {
             });
     });
 }
+    let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+
+    e.preventDefault();
+
+    deferredPrompt = e;
+
+    document.getElementById("installBtn").style.display = "inline-block";
+
+});
+
+document.getElementById("installBtn").addEventListener("click", async () => {
+
+    if (!deferredPrompt) return;
+
+    deferredPrompt.prompt();
+
+    await deferredPrompt.userChoice;
+
+    deferredPrompt = null;
+
+    document.getElementById("installBtn").style.display = "none";
+
+});
 
     displayHistory();
 
